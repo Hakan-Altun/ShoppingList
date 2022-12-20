3d990bc921805ad9472494cbe9385b81ab73580e
Hakan-Altun <115375517+Hakan-Altun@users.noreply.github.com>
2022-12-20 12:10:44 +0300

    Add files via upload

---
+++ ShoppingList/ViewComponents/MyList.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingList.ViewComponents
+{
+    public class MyList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            string value=HttpContext.Session.GetString("userEmail");
+            UserManager um = new UserManager(new EfUserRepository());
+            User user = um.GetAll().FirstOrDefault(a => a.UserEmail == value);
+            ListManager lm = new ListManager(new EfListRepository());
+            List<List> lists = lm.GetAll().Where(a => a.UserId == user.UserId).ToList();
+           
+            return View(lists); 
+        }
+    }
+}
